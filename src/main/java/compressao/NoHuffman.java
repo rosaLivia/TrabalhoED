55ecbe08316507f8a70f7f2a2a66d6664e39f001
Eos <100361496+theycelz@users.noreply.github.com>
2024-11-06 13:46:18 -0400

    feat: adicionar atributos e construtor à classe NoHuffman

--- src/main/java/compressao/NoHuffman.java
+++ src/main/java/compressao/NoHuffman.java
@@ -6,4 +6,21 @@ import java.util.HashMap;
 
 public class NoHuffman implements Comparable<NoHuffman> {
 
+    char caractere;
+    int frequencia;
+    NoHuffman esquerda;
+    NoHuffman direita;
+
+    public NoHuffman(char caractere, int frequencia) {
+        this.caractere = caractere;
+        this.frequencia = frequencia;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    @Override
+    public int compareTo(NoHuffman outro) {
+        return this.frequencia - outro.frequencia;
+    }
+
 }
