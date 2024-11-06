12e98484a6d030d9473249443a946f1ed2ed8825
Eos <100361496+theycelz@users.noreply.github.com>
2024-11-06 00:50:29 -0400

    feat: adicionar estrutura TrieNode com filhos, indicador de fim da palavra e conjunto de documentos

--- src/main/java/trie/TrieNode.java
+++ src/main/java/trie/TrieNode.java
@@ -1,5 +1,34 @@
 package main.java.trie;
 
-public class TrieNode {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class TrieNode {
+    private Map<Character, TrieNode> filhos;
+    private boolean fimDaPalavra;
+    private Set<String> documentos;
+
+    public TrieNode() {
+        this.filhos = new HashMap<>();
+        this.fimDaPalavra = false;
+        this.documentos = new HashSet<>();
+    }
+
+    public Map<Character, TrieNode> getFilhos() {
+        return filhos;
+    }
+
+    public boolean isFimDaPalavra() {
+        return fimDaPalavra;
+    }
+
+    public void setFimDaPalavra(boolean fimDaPalavra) {
+        this.fimDaPalavra = fimDaPalavra;
+    }
+
+    public Set<String> getDocumentos() {
+        return documentos;
+    }
 }
